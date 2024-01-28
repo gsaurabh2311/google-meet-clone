4aa116c81ca125df8891f890ae45e95cba3d35c1
Saurabh <gsaurabh.2311@gmail.com>
2024-01-28 12:44:38 +0530

    Setup Project and Socket

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useSockeet } from "@/context/socket"
+import { useEffect } from "react";
 
 export default function Home() {
+  const socket = useSockeet();
+
+  useEffect(() => {
+    socket?.on("connect", () => {
+      console.log(socket.id)
+    })
+  },[socket])
   return (
     <h1>Welcome</h1>
   )
